a1c4f69c737dd599093fa3bfa5262b21757e1bc8
Kkackit <kkackit@konkuk.ac.kr>
2022-01-02 22:04:33 +0900

    2022-01-02
    백준 10817 Code 커밋

--- Baekjoon/10817.cpp
+++ Baekjoon/10817.cpp
@@ -8,11 +8,31 @@ int main(void)
     int B = 0;
     int C = 0;
 
-    cin>>A;
-    cin>>B;
-    cin>>C;
+    int input_Array[3];
+    int sequence_Array[3];
+    cin>>input_Array[0];
+    cin>>input_Array[1];
+    cin>>input_Array[2];
 
+    int most_Num = 0;
+    int most_idx = 0;
 
-
+    for(int j = 0; j < 3; j++)
+    {
+        most_Num = 0;
+        most_idx = 0;
+        for(int i = 0; i < 3; i++)
+        {
+            if(input_Array[i] >= most_Num)
+            {
+                most_Num = input_Array[i];
+                most_idx = i;
+            }
+        }
+        input_Array[most_idx] = 0;
+        sequence_Array[j] = most_Num;
+    }
+    
+    cout<<sequence_Array[1]<<endl;
     
 }
